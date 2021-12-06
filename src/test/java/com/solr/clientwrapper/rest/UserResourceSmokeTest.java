73ca8440cb877f8db860faf0ff79ea8f28b43d24
Karthik Pillai <52003038+karthik261099@users.noreply.github.com>
2021-12-06 17:54:14 +0530

    Mocked Solr Core Service for passing test cases without live Solr Instance

--- src/test/java/com/solr/clientwrapper/rest/UserResourceSmokeTest.java
+++ src/test/java/com/solr/clientwrapper/rest/UserResourceSmokeTest.java
@@ -1,12 +1,12 @@
 package com.solr.clientwrapper.rest;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(properties = { "base-solr-url=http://localhost:8983/solr" })
 public class UserResourceSmokeTest {
 
 	@Autowired
