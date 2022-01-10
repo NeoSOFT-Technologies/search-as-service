17fff845e82789b0ae1e11f5b81bfcde7d63d913
Karthik Pillai <52003038+karthik261099@users.noreply.github.com>
2022-01-10 16:45:09 +0530

    Added Code to measure the time duration of the uploading process

--- src/main/java/com/solr/clientwrapper/rest/SolrInputDocumentResource.java
+++ src/main/java/com/solr/clientwrapper/rest/SolrInputDocumentResource.java
@@ -10,6 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Duration;
+import java.time.Instant;
+
 @RestController
 public class SolrInputDocumentResource {
 
@@ -27,7 +30,11 @@ public class SolrInputDocumentResource {
 
         log.debug("Solr documents add");
 
+        Instant start = Instant.now();
         SolrResponseDTO solrResponseDTO=createSolrDocument.addDocuments(collectionName, payload, isNRT);
+        Instant end = Instant.now();
+        Duration timeElapsed = Duration.between(start, end);
+        System.out.println("Time taken: "+ timeElapsed.toMillis() +" milliseconds or "+(timeElapsed.toMillis()/1000)+" seconds");
 
         if(solrResponseDTO.getStatusCode()==200){
             return ResponseEntity.status(HttpStatus.OK).body(solrResponseDTO);
