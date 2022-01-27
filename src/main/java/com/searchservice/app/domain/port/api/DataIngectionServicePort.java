2a061ce119c265c98389058915253f0a642c2584
Karthik Pillai <52003038+karthik261099@users.noreply.github.com>
2022-01-27 14:49:26 +0530

    Removed "DataIngection" from the project

--- src/main/java/com/searchservice/app/domain/port/api/DataIngectionServicePort.java
+++
@@ -1,6 +0,0 @@
-package com.searchservice.app.domain.port.api;
-
-public interface DataIngectionServicePort {	
-	String parseSolrSchemaArray(String collectionName, String jsonString);
-	String parseSolrSchemaBatch(String collectionName, String jsonString);
-}
