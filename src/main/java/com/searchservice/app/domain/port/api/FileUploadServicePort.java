84ad080f3fb56436f9737056524ae2393e0967b4
Karthik Pillai <52003038+karthik261099@users.noreply.github.com>
2022-01-27 14:53:43 +0530

    Removed "FileUpload" from the project

--- src/main/java/com/searchservice/app/domain/port/api/FileUploadServicePort.java
+++
@@ -1,9 +0,0 @@
-package com.searchservice.app.domain.port.api;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface FileUploadServicePort {
-
-	String multipartUploader(MultipartFile file);
-	
-}
