ebe8fc2029b09c08d820f329e644730524b23de3
skdhitman <piyushojha.skd@gmail.com>
2022-02-07 16:27:35 +0530

    Updated VersionedInputDocumentResource

--- src/main/java/com/searchservice/app/rest/VersionedInputDocumentResource.java
+++ src/main/java/com/searchservice/app/rest/VersionedInputDocumentResource.java
@@ -3,19 +3,19 @@ package com.searchservice.app.rest;
 
 import com.searchservice.app.domain.dto.ResponseDTO;
 import com.searchservice.app.domain.port.api.InputDocumentServicePort;
+import com.searchservice.app.rest.errors.InputDocumentException;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.Duration;
 import java.time.Instant;
 
 @RestController
-@RequestMapping("${base-url.api-endpoint.home}")
+@RequestMapping("/api")
 public class VersionedInputDocumentResource {
 
     private final Logger log = LoggerFactory.getLogger(VersionedInputDocumentResource.class);
@@ -26,27 +26,47 @@ public class VersionedInputDocumentResource {
         this.inputDocumentServicePort = inputDocumentServicePort;
     }
 
-    @PostMapping("/documents/{tableName}")
+
+
+    @PostMapping("/v1/ingest-nrt/{clientid}/{tableName}")
     @Operation(summary = "/ For add documents we have to pass the tableName and isNRT and it will return statusCode and message.", security = @SecurityRequirement(name = "bearerAuth"))
-    public ResponseEntity<ResponseDTO> documents(
-    		@PathVariable String tableName, 
-    		@RequestBody String payload) {
+    public ResponseDTO documents(@PathVariable String tableName,@PathVariable int clientid, @RequestBody String payload){
+
+        log.debug("Solr documents add");
+        tableName = tableName+"_"+clientid;
+        Instant start = Instant.now();
+        ResponseDTO solrResponseDTO= inputDocumentServicePort.addDocuments(tableName, payload);
+        Instant end = Instant.now();      
+        Duration timeElapsed = Duration.between(start, end);
+        String result="Time taken: "+timeElapsed.toMillis()+" milliseconds";
+        log.debug(result);
+
+        return solrResponseDTO;
+
+    }
+    
+
+	@PostMapping("/v1/ingest/{clientid}/{tableName}")
+    @Operation(summary = "/ For add documents we have to pass the tableName and isNRT and it will return statusCode and message.", security = @SecurityRequirement(name = "bearerAuth"))
+
+    public ResponseDTO document(@PathVariable String tableName,@PathVariable int clientid, @RequestBody String payload) {
+      
 
         log.debug("Solr documents add");
 
+        tableName = tableName+"_"+clientid;
         Instant start = Instant.now();
-        ResponseDTO solrResponseDTO= inputDocumentServicePort.addDocuments(tableName, payload);
+        ResponseDTO solrResponseDTO= inputDocumentServicePort.addDocument(tableName, payload);
         Instant end = Instant.now();
         Duration timeElapsed = Duration.between(start, end);
         String result="Time taken: "+timeElapsed.toMillis()+" milliseconds";
        log.debug(result);
 
         if(solrResponseDTO.getResponseStatusCode()==200){
-            return ResponseEntity.status(HttpStatus.OK).body(solrResponseDTO);
+            return solrResponseDTO;
         }else{
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(solrResponseDTO);
+        	throw new InputDocumentException(solrResponseDTO.getResponseStatusCode(),solrResponseDTO.getResponseMessage());
         }
-
     }
 
 
