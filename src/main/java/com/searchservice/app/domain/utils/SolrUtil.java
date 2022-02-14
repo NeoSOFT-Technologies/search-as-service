95e641b0b4b68c90265d4f136b8bd9631e538025
skdhitman <piyushojha.skd@gmail.com>
2022-02-14 15:03:46 +0530

    Added looger in SolrUtil

--- src/main/java/com/searchservice/app/domain/utils/SolrUtil.java
+++ src/main/java/com/searchservice/app/domain/utils/SolrUtil.java
@@ -4,13 +4,19 @@ import java.io.IOException;
 
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 
+import lombok.extern.slf4j.Slf4j;
 
+
+@Slf4j
 public class SolrUtil {
+	
+	private SolrUtil() {}
+	
 	public static void closeSolrClientConnection(HttpSolrClient solrClient) {
 		try {
 			solrClient.close();
 		} catch (IOException e) {
-			e.printStackTrace();
+			log.debug(e.getMessage());
 		}
 	}
 }
